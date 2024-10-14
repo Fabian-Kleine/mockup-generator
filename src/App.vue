9317f867d64f7872cb0cdca92bd7533b8beebfac
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-10-14 10:21:23 +0200

    setup basic layout with icons

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,67 @@
 <template>
-  <Button label="Submit" />
+  <div class="px-6 py-8 md:px-12 lg:px-20">
+        <div class="grid grid-cols-12 gap-4">
+            <div class="col-span-12 md:col-span-6 lg:col-span-3">
+                <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border">
+                    <div class="flex justify-between mb-4">
+                        <div>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">X (Twitter)</span>
+                            <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl"><Button label="Submit" /></div>
+                        </div>
+                        <div class="flex items-center justify-center bg-blue-100 dark:bg-blue-500/30 rounded-border w-10 h-10">
+                            <i class="pi pi-twitter text-blue-500 dark:text-blue-200 !text-xl" />
+                        </div>
+                    </div>
+                    <span class="text-green-500 font-medium">24 new </span>
+                    <span class="text-surface-500 dark:text-surface-300">since last visit</span>
+                </div>
+            </div>
+            <div class="col-span-12 md:col-span-6 lg:col-span-3">
+                <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border">
+                    <div class="flex justify-between mb-4">
+                        <div>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Revenue</span>
+                            <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl">$2.100</div>
+                        </div>
+                        <div class="flex items-center justify-center bg-red-100 dark:bg-red-400/30 rounded-border w-10 h-10">
+                            <i class="pi pi-youtube text-red-500 dark:text-red-200 !text-xl" />
+                        </div>
+                    </div>
+                    <span class="text-green-500 font-medium">%52+ </span>
+                    <span class="text-surface-500 dark:text-surface-300">since last week</span>
+                </div>
+            </div>
+            <div class="col-span-12 md:col-span-6 lg:col-span-3">
+                <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border">
+                    <div class="flex justify-between mb-4">
+                        <div>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Customers</span>
+                            <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl">28441</div>
+                        </div>
+                        <div class="w-10 h-10 flex items-center justify-center bg-green-100 dark:bg-green-400/30 rounded-border">
+                            <i class="pi pi-whatsapp text-green-500 dark:text-green-200 !text-xl" />
+                        </div>
+                    </div>
+                    <span class="text-green-500 font-medium">520 </span>
+                    <span class="text-surface-500 dark:text-surface-300">newly registered</span>
+                </div>
+            </div>
+            <div class="col-span-12 md:col-span-6 lg:col-span-3">
+                <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border">
+                    <div class="flex justify-between mb-4">
+                        <div>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Comments</span>
+                            <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl">152 Unread</div>
+                        </div>
+                        <div class="w-10 h-10 flex items-center justify-center bg-purple-100 dark:bg-purple-400/30 rounded-border">
+                            <i class="pi pi-discord text-purple-500 dark:text-purple-200 !text-xl" />
+                        </div>
+                    </div>
+                    <span class="text-green-500 font-medium">85 </span>
+                    <span class="text-surface-500 dark:text-surface-300">responded</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import PrimeVue from "primevue/config"
 import Button from "primevue/button"
 import Aura from "@primevue/themes/aura"
 import './style.css'
+import 'primeicons/primeicons.css'
 import App from './App.vue'
 
 const app = createApp(App);
