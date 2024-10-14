79bc011f24767a9f55c948f364507fb2b82d18e0
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-10-14 10:29:36 +0200

    setup vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import { createApp } from 'vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
 import PrimeVue from "primevue/config"
 import Button from "primevue/button"
 import Aura from "@primevue/themes/aura"
 import './style.css'
 import 'primeicons/primeicons.css'
 import App from './App.vue'
+import HomeView from './views/HomeView.vue'
+import TestView from './views/TestView.vue'
+
+const routes = [
+    { path: '/', component: HomeView },
+    { path: '/test', component: TestView }
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes
+});
 
 const app = createApp(App);
 
@@ -18,5 +31,7 @@ app.use(PrimeVue, {
         }
     }
 });
+
+app.use(router);
 
 app.mount('#app')
