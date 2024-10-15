3ddf2083cbc1edb344f2ddcc1497b52e2f166fe4
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-10-15 11:41:40 +0200

    update HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,13 @@
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-12 md:col-span-6 lg:col-span-3">
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
-                    <div class="flex justify-between mb-4">
-                        <div>
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">X (Twitter)</span>
+                    <div class="flex justify-between mb-4 h-full">
+                        <div class="flex flex-col h-full">
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">X (Twitter) tweet</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl">
-                                <img src="/x-mockup.png" alt="Mockup" class="mb-4">
-                                <Button as="router-link" label="Edit Mockup" to="/x" />
+                                <img src="/x-mockup.png" alt="Mockup" class="mb-4 w-[90%]">
                             </div>
+                            <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/x" />
                         </div>
                         <div class="flex items-center justify-center bg-blue-100 dark:bg-blue-500/30 rounded-border min-w-10 w-10 h-10">
                             <i class="pi pi-twitter text-blue-500 dark:text-blue-200 !text-xl" />
@@ -18,14 +18,14 @@
                 </div>
             </div>
             <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border">
-                    <div class="flex justify-between mb-4">
-                        <div>
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">YouTube</span>
+                <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
+                    <div class="flex justify-between mb-4 h-full">
+                        <div class="flex flex-col h-full">
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">YouTube Video</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl">
-                                <img src="/youtube-mockup.png" alt="Mockup" class="mb-4">
-                                <Button as="router-link" label="Edit Mockup" to="/youtube" />
+                                <img src="/youtube-mockup.png" alt="Mockup" class="mb-4 w-[90%]">
                             </div>
+                            <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/youtube" />
                         </div>
                         <div class="flex items-center justify-center bg-red-100 dark:bg-red-400/30 rounded-border min-w-10 w-10 h-10">
                             <i class="pi pi-youtube text-red-500 dark:text-red-200 !text-xl" />
