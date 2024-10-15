bf1b5f0bf3354d958e5978bd4ecfb16eeaa01175
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-10-15 10:11:06 +0200

    update user interface for editing X Mockup

--- src/views/XMockupView.vue
+++ src/views/XMockupView.vue
@@ -3,12 +3,29 @@ import { ref } from 'vue';
 import DatePicker from 'primevue/datepicker';
 import IftaLabel from 'primevue/iftalabel';
 import FileUpload from 'primevue/fileupload';
+import Select from 'primevue/select';
 import DomToImage from 'dom-to-image';
 
 const verified = ref(false);
+const background = ref("blue");
 const date = ref("Wed Oct 09 2024 21:43:52 GMT+0200 (Mitteleuropäische Sommerzeit)");
 const time = ref("Wed Oct 09 2024 21:43:52 GMT+0200 (Mitteleuropäische Sommerzeit)");
 const proiflePicture = ref("/x_placeholder.png");
+
+const backgroundOptions = [
+    {
+        color: 'White',
+        value: 'white'
+    },
+    {
+        color: 'Blue',
+        value: 'blue'
+    },
+    {
+        color: 'None',
+        value: 'none'
+    }
+]
 
 function onSelect(e) {
     proiflePicture.value = e.files[0].objectURL;
@@ -25,9 +42,42 @@ async function download() {
 </script>
 
 <template>
-    <div class="w-full min-h-screen flex justify-center items-center flex-col">
-        <div class="col-span-12 md:col-span-6 lg:col-span-3">
-            <div class="bg-white p-4 rounded-border w-[600px]" id="capture">
+    <div class="w-full min-h-screen block xl:grid grid-cols-4 gap-4">
+        <div class="flex flex-col gap-4 mt-4 px-4 edit-tweet mb-10 xl:mb-0">
+            <h2 class="text-xl font-bold">Edit Tweet Mockup</h2>
+            <div class="flex items-center">
+                <Checkbox v-model="verified" binary inputId="verified" name="verified" value="Verified" />
+                <label for="verified" class="ml-2"> Verified </label>
+            </div>
+            <IftaLabel>
+                <DatePicker v-model="date" fluid inputId="date" showIcon iconDisplay="input" variant="filled"
+                    dateFormat="M dd, yy" />
+                <label for="date">Date</label>
+            </IftaLabel>
+            <IftaLabel>
+                <DatePicker v-model="time" fluid inputId="time" showIcon iconDisplay="input" variant="filled" timeOnly
+                    hourFormat="12">
+                    <template #inputicon="slotProps">
+                        <i class="pi pi-clock" @click="slotProps.clickCallback" />
+                    </template>
+                </DatePicker>
+                <label for="date">Time</label>
+            </IftaLabel>
+            <IftaLabel>
+                <Select inputId="background" v-model="background" :options="backgroundOptions" optionLabel="color" optionValue="value"
+                    placeholder='Select a Background' class="w-full" />
+                <label for="background">Background</label>
+            </IftaLabel>
+            <FileUpload mode="basic" accept="image/*" @select="onSelect" chooseLabel="Upload Profile Picture" />
+            <div class="flex gap-2 mt-4">
+                <Button as="router-link" class="flex-1" label="Cancel" severity="secondary" to="/" />
+                <Button class="flex-1" label="Download" severity="success" @click="download" />
+            </div>
+        </div>
+        <div
+            :class="['relative col-span-3 flex justify-center items-center min-h-[800px]', background == 'white' ? 'bg-white' : background == 'blue' ? 'bg-sky-400' : '']" :id="background == 'white' || background == 'blue' ? 'capture' : ''">
+            <div :class="['bg-white p-4 rounded-border w-[600px]', background == 'white' || background == 'blue' ? 'shadow-2xl' : '']"
+                :id="background != 'white' && background != 'blue' ? 'capture' : ''">
                 <div class="flex justify-between mb-4">
                     <div class="w-full">
                         <div class="flex justify-between items-center">
@@ -80,30 +130,8 @@ async function download() {
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="flex flex-col gap-4 w-[600px] mt-4">
-            <div class="flex items-center">
-                <Checkbox v-model="verified" binary inputId="verified" name="verified" value="Verified" />
-                <label for="verified" class="ml-2"> Verified </label>
-            </div>
-            <IftaLabel>
-                <DatePicker v-model="date" inputId="date" showIcon iconDisplay="input" variant="filled"
-                    dateFormat="M dd, yy" />
-                <label for="date">Date</label>
-            </IftaLabel>
-            <IftaLabel>
-                <DatePicker v-model="time" inputId="time" showIcon iconDisplay="input" variant="filled" timeOnly
-                    hourFormat="12">
-                    <template #inputicon="slotProps">
-                        <i class="pi pi-clock" @click="slotProps.clickCallback" />
-                    </template>
-                </DatePicker>
-                <label for="date">Time</label>
-            </IftaLabel>
-            <FileUpload mode="basic" accept="image/*" @select="onSelect" chooseLabel="Upload Profile Picture" />
-            <div class="flex gap-2 mt-4">
-                <Button as="router-link" class="flex-1" label="Cancel" severity="secondary" to="/" />
-                <Button class="flex-1" label="Download" severity="success" @click="download" />
+            <div v-if="background == 'white' || background == 'blue'" class="absolute bottom-10 left-10 w-14 h-14 rounded-full bg-black flex justify-center items-center">
+                <i class="pi pi-twitter !text-2xl"></i>
             </div>
         </div>
     </div>
