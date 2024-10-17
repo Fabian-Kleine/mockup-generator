523ecee8fd1845bfa2f3509d81c4211f2092193b
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-10-17 11:43:01 +0200

    add tiktok mockup image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -91,7 +91,7 @@
                         <div class="flex flex-col h-full">
                             <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Tiktok</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
-                                <img src="/instagram-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
+                                <img src="/tiktok-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/tiktok" />
                         </div>
