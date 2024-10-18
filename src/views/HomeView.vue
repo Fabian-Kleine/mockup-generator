712f2068361b16d8305ee871644574a6a9ab0030
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-10-18 08:45:14 +0200

    add hero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,42 @@
 <template>
-      <div class="px-6 py-8 md:px-12 lg:px-20 max-w-screen overflow-x-hidden">
-        <div class="flex items-center gap-2 mb-4">
-            <img src="/Mockup-generator-logo.png" alt="Logo" width="50">
-            <h1 class="text-xl font-bold">Mockup Creator</h1>
+    <div class="flex lg:flex-row flex-col gap-4">
+        <div class="flex-1 flex items-center justify-center">
+            <div class="p-6 pt-12 lg:p-12">
+                <h1
+                    class="text-3xl lg:text-5xl font-bold text-surface-900 dark:text-surface-0 mb-4 lg:leading-normal text-center lg:text-left">
+                    Mockup Creator
+                </h1>
+                <p class="text-surface-700 dark:text-surface-200 leading-normal text-center lg:text-left invisible h-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua.
+                </p>
+                <div class="flex items-center justify-center lg:justify-start gap-6">
+                    <Button as="a" href="#mockups" label="All Mockups" type="button" />
+                    <Button as="router-link" to="/browser" label="Browser Mockups" type="button" outlined />
+                </div>
+            </div>
         </div>
+        <div class="flex-1 overflow-hidden">
+            <img src="/hero.png"
+                alt="hero-1"
+                class="h-full w-full object-cover lg:[clip-path:polygon(12%_0,100%_0%,100%_100%,0_100%)]" />
+        </div>
+    </div>
+    <div class="px-6 py-8 md:px-12 lg:px-20 max-w-screen overflow-x-hidden" id="mockups">
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-12 md:col-span-6 lg:col-span-3">
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">X (Twitter) tweet</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">X (Twitter)
+                                tweet</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/x-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/x" />
                         </div>
-                        <div class="flex items-center justify-center bg-blue-100 dark:bg-blue-500/30 rounded-border min-w-10 w-10 h-10">
+                        <div
+                            class="flex items-center justify-center bg-blue-100 dark:bg-blue-500/30 rounded-border min-w-10 w-10 h-10">
                             <i class="pi pi-twitter text-blue-500 dark:text-blue-200 !text-xl" />
                         </div>
                     </div>
@@ -25,13 +46,15 @@
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">YouTube Video</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">YouTube
+                                Video</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/youtube-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/youtube" />
                         </div>
-                        <div class="flex items-center justify-center bg-red-100 dark:bg-red-400/30 rounded-border min-w-10 w-10 h-10">
+                        <div
+                            class="flex items-center justify-center bg-red-100 dark:bg-red-400/30 rounded-border min-w-10 w-10 h-10">
                             <i class="pi pi-youtube text-red-500 dark:text-red-200 !text-xl" />
                         </div>
                     </div>
@@ -41,13 +64,15 @@
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">YouTube Comment</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">YouTube
+                                Comment</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/youtube-comment-mockup.png" alt="Mockup" class="rounded-md">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/youtube-comment" />
                         </div>
-                        <div class="flex items-center justify-center bg-red-100 dark:bg-red-400/30 rounded-border min-w-10 w-10 h-10">
+                        <div
+                            class="flex items-center justify-center bg-red-100 dark:bg-red-400/30 rounded-border min-w-10 w-10 h-10">
                             <i class="pi pi-youtube text-red-500 dark:text-red-200 !text-xl" />
                         </div>
                     </div>
@@ -57,13 +82,15 @@
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Whatsapp Chat</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Whatsapp
+                                Chat</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/whatsapp-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/whatsapp" />
                         </div>
-                        <div class="w-10 h-10 min-w-10 flex items-center justify-center bg-green-100 dark:bg-green-400/30 rounded-border">
+                        <div
+                            class="w-10 h-10 min-w-10 flex items-center justify-center bg-green-100 dark:bg-green-400/30 rounded-border">
                             <i class="pi pi-whatsapp text-green-500 dark:text-green-200 !text-xl" />
                         </div>
                     </div>
@@ -73,13 +100,15 @@
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Instagram Post</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Instagram
+                                Post</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/instagram-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/instagram" />
                         </div>
-                        <div class="w-10 h-10 min-w-10 flex items-center justify-center bg-fuchsia-100 dark:bg-fuchsia-400/30 rounded-border">
+                        <div
+                            class="w-10 h-10 min-w-10 flex items-center justify-center bg-fuchsia-100 dark:bg-fuchsia-400/30 rounded-border">
                             <i class="pi pi-instagram text-fuchsia-500 dark:text-fuchsia-200 !text-xl" />
                         </div>
                     </div>
@@ -95,7 +124,8 @@
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/tiktok" />
                         </div>
-                        <div class="w-10 h-10 min-w-10 flex items-center justify-center bg-pink-100 dark:bg-pink-400/30 rounded-border">
+                        <div
+                            class="w-10 h-10 min-w-10 flex items-center justify-center bg-pink-100 dark:bg-pink-400/30 rounded-border">
                             <i class="pi pi-tiktok text-pink-500 dark:text-pink-200 !text-xl" />
                         </div>
                     </div>
@@ -105,13 +135,15 @@
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Phone Mockup</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Phone
+                                Mockup</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/phone-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/phone" />
                         </div>
-                        <div class="w-10 h-10 min-w-10 flex items-center justify-center bg-amber-100 dark:bg-amber-400/30 rounded-border">
+                        <div
+                            class="w-10 h-10 min-w-10 flex items-center justify-center bg-amber-100 dark:bg-amber-400/30 rounded-border">
                             <i class="pi pi-mobile text-amber-500 dark:text-amber-200 !text-xl" />
                         </div>
                     </div>
@@ -121,13 +153,15 @@
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Phone Lockscreen Mockup</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Phone Lockscreen
+                                Mockup</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/lockscreen-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/lockscreen" />
                         </div>
-                        <div class="w-10 h-10 min-w-10 flex items-center justify-center bg-amber-100 dark:bg-amber-400/30 rounded-border">
+                        <div
+                            class="w-10 h-10 min-w-10 flex items-center justify-center bg-amber-100 dark:bg-amber-400/30 rounded-border">
                             <i class="pi pi-mobile text-amber-500 dark:text-amber-200 !text-xl" />
                         </div>
                     </div>
@@ -137,13 +171,15 @@
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Image Mockup</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Image
+                                Mockup</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/image-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/image" />
                         </div>
-                        <div class="w-10 h-10 min-w-10 flex items-center justify-center bg-pink-100 dark:bg-pink-400/30 rounded-border">
+                        <div
+                            class="w-10 h-10 min-w-10 flex items-center justify-center bg-pink-100 dark:bg-pink-400/30 rounded-border">
                             <i class="pi pi-image text-pink-500 dark:text-pink-200 !text-xl" />
                         </div>
                     </div>
@@ -153,13 +189,15 @@
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Browser Mockup</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Browser
+                                Mockup</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/browser-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/browser" />
                         </div>
-                        <div class="w-10 h-10 min-w-10 flex items-center justify-center bg-pink-100 dark:bg-pink-400/30 rounded-border">
+                        <div
+                            class="w-10 h-10 min-w-10 flex items-center justify-center bg-pink-100 dark:bg-pink-400/30 rounded-border">
                             <i class="pi pi-globe text-pink-500 dark:text-pink-200 !text-xl" />
                         </div>
                     </div>
@@ -169,31 +207,18 @@
                 <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border h-full">
                     <div class="flex justify-between mb-4 h-full">
                         <div class="flex flex-col h-full">
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Window Mockup</span>
+                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Window
+                                Mockup</span>
                             <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl flex justify-center">
                                 <img src="/window-mockup.png" alt="Mockup" class="rounded-md w-[90%]">
                             </div>
                             <Button class="mt-auto" as="router-link" label="Edit Mockup" to="/window" />
                         </div>
-                        <div class="w-10 h-10 min-w-10 flex items-center justify-center bg-pink-100 dark:bg-pink-400/30 rounded-border">
+                        <div
+                            class="w-10 h-10 min-w-10 flex items-center justify-center bg-pink-100 dark:bg-pink-400/30 rounded-border">
                             <i class="pi pi-window-maximize text-pink-500 dark:text-pink-200 !text-xl" />
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                <div class="bg-surface-0 dark:bg-surface-900 shadow p-4 rounded-border">
-                    <div class="flex justify-between mb-4">
-                        <div>
-                            <span class="block text-surface-500 dark:text-surface-300 font-medium mb-4">Comments</span>
-                            <div class="text-surface-900 dark:text-surface-0 font-medium !text-xl">152 Unread</div>
-                        </div>
-                        <div class="w-10 h-10 flex items-center justify-center bg-purple-100 dark:bg-purple-400/30 rounded-border">
-                            <i class="pi pi-discord text-purple-500 dark:text-purple-200 !text-xl" />
-                        </div>
-                    </div>
-                    <span class="text-green-500 font-medium">85 </span>
-                    <span class="text-surface-500 dark:text-surface-300">responded</span>
                 </div>
             </div>
         </div>
