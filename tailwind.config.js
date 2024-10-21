00f2d8a17af7d6859477e73c878f785b79b81d5b
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-10-21 11:09:30 +0200

    add spotify mockup editor

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         'instagram-gradient': 'linear-gradient(45deg, #f09433, #e6683c, #dc2743, #cc2366, #bc1888)',
         'tiktok-gradient': 'linear-gradient(45deg, #69C9D0, #000000 40%, #000000 60%, #EE1D52)',
       },
+      colors: {
+        'variable': "var(--custom-color)"
+      },
     },
   },
   plugins: [require('tailwindcss-primeui')]
