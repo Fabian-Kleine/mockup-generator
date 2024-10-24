ae96956cfa17c29df9d0bcada220cb6e2a8771f2
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-10-24 08:30:06 +0200

    update downloadCapture function
    change downloadCapture function to set the capture container to a specific width and/or height to always export images with the same size

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,14 +1,27 @@
 import DomToImage from 'dom-to-image';
-
-export default async function delay(ms) {
-    await new Promise(resolve => setTimeout(resolve, ms));
-}
-
 export default async function downloadCapture(filename) {
-    const dataUrl = await DomToImage.toPng(document.querySelector('#capture'));
+    const capture = document.querySelector('#capture');
+
+    if (!capture) {
+        return;
+    }
+
+    if (capture.classList.contains('capture-container')) {
+        capture.setAttribute('style', 'width: 1436px; height: 945px;');
+    }
+
+    if (capture.classList.contains('capture-container-video')) {
+        capture.setAttribute('style', 'width: 1920px;');
+        capture.children[1].setAttribute('style', 'display: grid;');
+    }
+
+    const dataUrl = await DomToImage.toPng(capture);
 
     const link = document.createElement("a");
     link.download = filename;
     link.href = dataUrl;
     link.click();
+
+    capture.removeAttribute('style');
+    capture.children[1].removeAttribute('style');
 }
