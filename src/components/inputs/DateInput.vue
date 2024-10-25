3124c0086563273a8b1088b74a8b28c3150defc4
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-10-25 10:51:09 +0200

    add DateInput component

---
+++ src/components/inputs/DateInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="js">
+    import IftaLabel from 'primevue/iftalabel';
+    import DatePicker from 'primevue/datepicker';
+
+    import { ref, watch } from 'vue';
+
+    const emit = defineEmits(['update:date']);
+
+    const date = ref(new Date(2024, 10, 9));
+
+    watch(() => date.value, (newVal) => {
+        emit('update:date', newVal);
+    });
+</script>
+
+<template>
+    <IftaLabel>
+        <DatePicker v-model="date" fluid inputId="date" showIcon iconDisplay="input" variant="filled"
+            dateFormat="M dd, yy" />
+        <label for="date">Date</label>
+    </IftaLabel>
+</template>
