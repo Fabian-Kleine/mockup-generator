176e710aac069aec18b33ae4039f359958266021
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-10-25 10:39:03 +0200

    add TimeInput component

---
+++ src/components/inputs/TimeInput.vue
@@ -0,0 +1,34 @@
+<script setup lang="js">
+    import IftaLabel from 'primevue/iftalabel';
+    import DatePicker from 'primevue/datepicker';
+
+    import { ref, watch } from 'vue';
+
+    const props = defineProps({
+        hourFormat: {
+            type: String,
+            default: '12'
+        }
+    })
+
+    const emit = defineEmits(['update:time']);
+
+    const time = ref(new Date());
+    time.value.setHours(11, 43, 0);
+
+    watch(() => time.value, (newVal) => {
+        emit('update:time', newVal);
+    });
+</script>
+
+<template>
+    <IftaLabel>
+        <DatePicker v-model="time" fluid inputId="time" showIcon iconDisplay="input" variant="filled" timeOnly
+            :hourFormat="hourFormat">
+            <template #inputicon="slotProps">
+                <i class="pi pi-clock" @click="slotProps.clickCallback" />
+            </template>
+        </DatePicker>
+        <label for="date">Time</label>
+    </IftaLabel>
+</template>
